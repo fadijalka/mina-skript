73a3e7d76a024e36f2762cf442d358cb455b5e9f
fadijalka <fadi.jalka@yh.nackademin.se>
2019-01-07 12:32:03 +0100

    my 2 commit

---
+++ skript2019-01-03a.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+FILES=/etc/*
+file_exists=0
+date_today=$(date -d "now" +"%Y-%m-%d" )
+for f in $FILES
+do
+	lastModified=$(stat $f | awk '$1 == "Modify:" {print $2}')
+	compareDate=$(date -d "-2 days" +%s)
+	lastModifiedDate=$(date -d $lastModified +%s)
+	if [ $lastModifiedDate -ge $compareDate ]; then
+		if [ $file_exists -eq 0 ]; then
+			zip "$date_today.zip" anyfile
+			zip -d "$date_today".zip anyfile
+			file_exists=1
+		fi
+		zip -rv "$date_today.zip" "$f"
+	fi
+done
---
+++ skript2019-01-03b.sh
@@ -0,0 +1,2 @@
+!/bin/bash
+tar -cvf backup-$(date &apos;+%Y&apos;).tar backup</pre>
